4373bc48f40c12ab40b10300a77613e560ec98cc
yunze <wangyunze412@gmail.com>
2025-11-18 13:23:28 +0800

    neo-tree: show hidden files by default

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "vim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+    },
+  },
+}
