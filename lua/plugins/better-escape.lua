8fed60f1f23dbb91f17be00d48bfbd4ff3a83668
yunze <wangyunze412@gmail.com>
2026-02-18 12:21:51 +0800

    make capital JK also exit insert mode

--- lua/plugins/better-escape.lua
+++ lua/plugins/better-escape.lua
@@ -1,9 +1,9 @@
 return {
   "max397574/better-escape.nvim",
   event = "VeryLazy",
-  opts = function (_, opts)
+  opts = function(_, opts)
     opts.mappings = {
-      i = { j = { k = "<Esc>" } },
+      i = { j = { k = "<Esc>" }, J = { K = "<Esc>" } },
     }
   end,
 }
